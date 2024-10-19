aa73c1b14c6428475b5857da1f26f24920b0afcc
Marco Baratto <marcobaratto@icloud.com>
2024-10-19 15:04:30 +0200

    Init jest config file and imported main dependencies

---
+++ jest.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from 'jest';
+import nextJest from 'next/jest.js'
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './',
+})
+
+const config: Config = {
+  clearMocks: true,
+  collectCoverage: false,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  testEnvironment: "jsdom",
+  verbose: true,
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts']
+};
+
+export default createJestConfig(config);
